152b26a7a04512df0e9f5c5921d6a2b35e3b9aca
thulanimbatha <thulanimbatha06@gmail.com>
2022-10-05 14:33:20 +0200

    created a graph

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
+from urllib import request
 import requests
 
 API_ENDPOINT = "https://pixe.la/v1/users"
+USERNAME = "thulani"
+TOKEN = "AtomicHabits"
 
-parameters = {
-    "token" : "AtomicHabits",
-    "username" : "thulani",
+graph_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs"
+
+user_parameters = {
+    "token" : TOKEN,
+    "username" : USERNAME,
     "agreeTermsOfService" : "yes",
     "notMinor" : "yes"
 
 }
 
-# create user account
-response = requests.post(url=API_ENDPOINT, json=parameters)
+graph_parameters = {
+    "id"    : "codinghabit2022",
+    "name"  : "Coding Graph",
+    "unit"  : "hrs",
+    "type"  : "float",
+    "color" : "ichou",
+}
+
+headers = {
+    "X-USER-TOKEN" : TOKEN,
+}
+
+# # create user account
+# response = requests.post(url=API_ENDPOINT, json=parameters)
+# print(response.text)
+
+# create a graph definition
+response = requests.post(url=graph_endpoint, json=graph_parameters, headers=headers)
 print(response.text)
